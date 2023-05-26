33db322f9c05078d57fd3dde6c22022c37705dce
beguene <beguene@gmail.com>
2023-05-26 13:02:27 +0000

    fix: rules

--- src/pages/rules.tsx
+++ src/pages/rules.tsx
@@ -88,16 +88,25 @@ function Rules() {
   
     const data = await response.json();
     console.log('data from getSTXRule', data)
-    const result = cvToString(hexToCV(data.result))
+    const result = hexToCV(data.result)
     return result
   }
 
-  const [stxRules, setStxRules] = useState([])
-    const rules = {"okay": true, "result": "0x0b000000010c000000040c616d6f756e742d6f722d69640100000000000000000000000005f5e100056173736574090269640100000000000000000000000000000000046b696e640100000000000000000000000000000000"}
+  const [stxRules, setStxRules] = useState()
+  // const rules = {"okay": true, "result": "0x0b000000010c000000040c616d6f756e742d6f722d69640100000000000000000000000005f5e100056173736574090269640100000000000000000000000000000000046b696e640100000000000000000000000000000000"}
   useEffect(() => {
     if (!testnetAddress) return;
     async function getRules() {
       const result = await getSTXRules();
+      console.log('got result', result);
+      const formatted = result.list.map((item) => {
+        const amountOrId = cvToString(item.data['amount-or-id']);
+        const asset = cvToString(item.data.asset)
+        const id = cvToString(item.data.id)
+        const kind = cvToString(item.data.kind)
+        return { amountOrId, asset, id, kind }
+      });
+      console.log('formatted', formatted);
       setStxRules(result);
     }
     getRules();
@@ -134,12 +143,33 @@ function Rules() {
       mb={8}
       w="full"
     >
-      <Box>
-        {stxRules && stxRules.length > 0 ? (
-          <Text fontSize="xl" fontWeight="bold">
-            Rules: {stxRules}
-          </Text>) : null}
-      </Box>
+      {stxRules && stxRules?.length > 0 ? (
+        <>
+        <Text>Active Rules</Text>
+        <VStack spacing={4} align="stretch">
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Txid</Th>
+                <Th>Recipient Address</Th>
+              </Tr>
+            </Thead>
+              <Tbody>
+                <>
+                  {stxRules && stxRules?.length > 0 && stxRules.map((item) => {
+                    return (<Tr key={item} >
+                      <Td>{item['amountOrId']}</Td>
+                      <Td>{item['asset']}</Td>
+                      <Td>{item['id']}</Td>
+                      <Td>{item['kind']}</Td>
+                    </Tr>)
+                  })}
+                </>
+              </Tbody>
+            </Table>
+        </VStack>
+        </>
+      ) : null}
 
       {signers && signers.length > 0 ? (
         <VStack spacing={4} align="stretch">
