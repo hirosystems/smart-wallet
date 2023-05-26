b0049c916afbfc4dcd7ea8ff9fb0e55ebc3550b4
beguene <beguene@gmail.com>
2023-05-26 13:23:04 +0000

    fix send message

--- src/pages/api/send-message.ts
+++ src/pages/api/send-message.ts
@@ -6,7 +6,7 @@ import { signers } from './store';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { stxAddress, txId } = req.body;
-    console.log('stxAddress', stxAddress);
+    console.log('stxAddress', stxAddress, req.body, req);
     if (!stxAddress) {
       res
         .status(400)
